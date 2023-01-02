25c7333eb79512fa9926b121da5ea9d38fb519d8
Obanijesu <obanijesujohn45@gmail.com>
2023-01-02 13:10:22 +0100

    new update

--- src/components/mainpage/SlideCard.jsx
+++ src/components/mainpage/SlideCard.jsx
@@ -3,7 +3,12 @@ import React from "react";
 function SlideCard() {
   return (
     <>
-      <h1>cad</h1>
+      {}
+      <div className="box d_flex top">
+        <div className="left">
+          <h1>{}</h1>
+        </div>
+      </div>
     </>
   );
 }
