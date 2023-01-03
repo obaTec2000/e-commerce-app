d19199bf9a9bf06b5de8f6248b092d16443b4fa9
Obanijesu <obanijesujohn45@gmail.com>
2023-01-03 22:25:01 +0100

    Sdata update

--- src/components/mainpage/Sdata.js
+++ src/components/mainpage/Sdata.js
@@ -17,11 +17,5 @@ const Sdata = [
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
     cover: "./images/SlideCard/slide-3.png",
   },
-  {
-    id: 4,
-    title: "50% Off For Your First Shopping",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-    cover: "./images/SlideCard/slide-4.png",
-  },
 ];
 export default Sdata;
