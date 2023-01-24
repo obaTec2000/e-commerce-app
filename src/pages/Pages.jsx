6494153ef053c5667dc89f18eebf701868de67f5
Obanijesu <obanijesujohn45@gmail.com>
2023-01-25 00:43:13 +0100

    New Arrival

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import Home from "../components/mainPage/Home"
 import FlashDeals from "../components/flashDeals/FlashDeals"
 import TopCate from "../components/top/TopCate"
-// import NewArrivals from "../components/newarrivals/NewArrivals"
+import NewArrivals from "../components/newarrivals/NewArrivals"
 // import Discount from "../components/discount/Discount"
 // import Shop from "../components/shops/Shop"
 // import Annocument from "../components/annocument/Annocument"
@@ -14,8 +14,8 @@ const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
       <Home CartItem={CartItem} />  
       <FlashDeals productItems={productItems} addToCart={addToCart} />
       <TopCate />
-      {/* <NewArrivals />
-      <Discount />
+      <NewArrivals />
+      {/* <Discount />
       <Shop shopItems={shopItems} addToCart={addToCart} />
       <Annocument />
       <Wrapper /> */}
