7f7a8e438be074d8d191b98c0dd81c22630c9d75
Obanijesu <obanijesujohn45@gmail.com>
2023-01-25 00:50:07 +0100

    Discount

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,7 +3,7 @@ import Home from "../components/mainPage/Home"
 import FlashDeals from "../components/flashDeals/FlashDeals"
 import TopCate from "../components/top/TopCate"
 import NewArrivals from "../components/newarrivals/NewArrivals"
-// import Discount from "../components/discount/Discount"
+import Discount from "../components/discount/Discount"
 // import Shop from "../components/shops/Shop"
 // import Annocument from "../components/annocument/Annocument"
 // import Wrapper from "../components/wrapper/Wrapper"
@@ -15,10 +15,10 @@ const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
       <FlashDeals productItems={productItems} addToCart={addToCart} />
       <TopCate />
       <NewArrivals />
-      {/* <Discount />
-      <Shop shopItems={shopItems} addToCart={addToCart} />
-      <Annocument />
-      <Wrapper /> */}
+      <Discount />
+      {/* <Shop shopItems={shopItems} addToCart={addToCart} /> */}
+      {/* <Annocument /> */}
+      {/* <Wrapper /> */}
     </>
   )
 }
