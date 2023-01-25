996d95a9d4ddc34d69c8a05b6424efe599a29e36
Obanijesu <obanijesujohn45@gmail.com>
2023-01-25 01:15:41 +0100

    Shops Items

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Pages from "./pages/Pages"
 import Data from "./components/Data"
 import Cart from "./common/cart/Cart.jsx"
 // import Footer from "./common/footer/Footer"
-// import Sdata from "./components/shops/Sdata"
+import Sdata from "./components/shops/Sdata"
 
 function App() {
 
   
   const { productItems } = Data
-  // const { shopItems } = Sdata
+  const { shopItems } = Sdata
 
  
   const [CartItem, setCartItem] = useState([])
@@ -21,7 +21,7 @@ function App() {
   const addToCart = (product) => {
   
     const productExit = CartItem.find((item) => item.id === product.id)
-    
+     
     if (productExit) {
       setCartItem(CartItem.map((item) => (item.id === product.id ? { ...productExit, qty: productExit.qty + 1 } : item)))
     } else {
@@ -50,7 +50,7 @@ function App() {
         <Header CartItem={CartItem} />
         <Switch>
           <Route path='/' exact>
-            <Pages productItems={productItems} addToCart={addToCart}  />
+            <Pages productItems={productItems} addToCart={addToCart} shopItems={shopItems} />
           </Route>
           <Route path='/cart' exact>
             <Cart CartItem={CartItem} addToCart={addToCart} decreaseQty={decreaseQty} />
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -4,7 +4,7 @@ import FlashDeals from "../components/flashDeals/FlashDeals"
 import TopCate from "../components/top/TopCate"
 import NewArrivals from "../components/newarrivals/NewArrivals"
 import Discount from "../components/discount/Discount"
-// import Shop from "../components/shops/Shop"
+import Shop from "../components/shops/Shop"
 // import Annocument from "../components/annocument/Annocument"
 // import Wrapper from "../components/wrapper/Wrapper"
 
@@ -16,7 +16,7 @@ const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
       <TopCate />
       <NewArrivals />
       <Discount />
-      {/* <Shop shopItems={shopItems} addToCart={addToCart} /> */}
+      <Shop shopItems={shopItems} addToCart={addToCart} />
       {/* <Annocument /> */}
       {/* <Wrapper /> */}
     </>
